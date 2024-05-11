d6d5ac61d14877a74d5d08712e2f917b66755aac
6Glow <bampoothe@gmail.com>
2024-05-11 16:00:51 +0300

    ERC-20 token created

---
+++ contracts/MyScooby.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract MyScooby is ERC20 {
+
+    constructor() ERC20("ScoobyCoin", "SCOOBY") {
+        _mint(msg.sender, 100 * 10 ** ERC20.decimals());
+    }
+}
